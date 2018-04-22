042a0e31cb4d9a253791c5221213dcdc82d50595
jmcass0342 <jmc90180@uga.edu>
2018-04-22 16:14:08 -0400

    server setup

---
+++ server/app.js
@@ -0,0 +1,4 @@
+var express = require('express');
+var app = express();
+app.get('/', function (req, res) { return res.send('Hello World!'); });
+app.listen(3000, function () { return console.log('Example app listening on port 3000!'); });
