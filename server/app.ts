d8f283cc856efb3db8c1102b867f3b6d71ca365c
jmcass0342 <jmc90180@uga.edu>
2018-04-22 16:56:35 -0400

    setting up mysql connection

--- server/app.ts
+++ server/app.ts
@@ -1,6 +1,7 @@
 const express = require('express');
+const dbcheck = require('./dbconnection');
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.send(dbcheck.connection()));
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
